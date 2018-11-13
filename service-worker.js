6ece5229789c1207c3ed7c04fe1ababff10aa269
Dean <deanhume@gmail.com>
2018-11-13 17:12:51 +0000

    Added timeout functionality

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,47 @@
 "use strict";
 
+/**
+ * Force a timeout if we go beyond given time period
+ * @param {int} delay 
+ */
+function timeout(delay) {
+    return new Promise(function(resolve, reject) {
+        setTimeout(function(){
+          resolve();
+        }, delay);
+    });
+}
+
+/**
+ * Fetch the image from Cloudinary and
+ * fail if there are any errors.
+ * @param {string} imageUrl 
+ */
+function fetchCloudinaryImage(imageUrl){
+
+    // Build up the Cloundinary URL
+    const cloudinaryUrl = `https://res.cloudinary.com/hume/image/fetch/q_auto,f_auto/${imageUrl}`;    
+    
+    return fetch(cloudinaryUrl, response => {
+        if (!response.ok){
+            // We failed return original image
+            console.log(response.ok);
+        }
+
+        return response;
+    }).catch(error => {
+        console.log(error);
+    })
+
+}
+
 self.addEventListener('fetch', event => {
 
     // Check if the request is for an image
     if (/\.jpg$|.png$|.gif$/.test(event.request.url)) {
 
-        // Get the existing image URL
-        let imageUrl = event.request.url;
-
-        // Build up the Cloundinary URL
-        const cloudinaryUrl = `https://res.cloudinary.com/hume/image/fetch/q_auto,f_auto/${imageUrl}`;
-
-        // Try and fetch the image
-        event.respondWith(
-            fetch(cloudinaryUrl, response => {
-                if (!response.ok){
-                    // We failed return original image
-                    console.log(response.ok);
-                }
-
-                return response;
-            }).catch(error => {
-                console.log(error);
-            })
-        );
-
-        // TODO: Add timeout and fallback
+        // Try and fetch the image / timeout if too slow
+        event.respondWith(Promise.race([timeout(2000), fetchCloudinaryImage(event.request.url)]));
     }
 });
 
